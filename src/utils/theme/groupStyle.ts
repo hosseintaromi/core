cd233331a2713ca7140e6cba1987028b1ddfb3e0
sr-ssh <r.sadatshokouhi@gmail.com>
2023-11-14 19:05:57 +0330

    feat: :lipstick: add groups style

---
+++ src/utils/theme/groupStyle.ts
@@ -0,0 +1,23 @@
+import { ControlStyleType } from "../../@types/ThemeTypes";
+
+const groupStyle = (groupStyle?: ControlStyleType) => {
+  const border = groupStyle?.border;
+  return {
+    marginBlock: groupStyle?.margin?.horizontal + "px",
+    marginInline: groupStyle?.margin?.vertical + "px",
+    backgroundColor: groupStyle?.background_color,
+    fontSize: groupStyle?.font_size + "px",
+    fontWeight: groupStyle?.font_weight,
+    color: groupStyle?.text_color,
+    borderTop: border?.top,
+    borderBottom: border?.bottom,
+    borderRight: border?.right,
+    borderLeft: border?.left,
+    borderRadius: groupStyle?.radius,
+    boxShadow: groupStyle?.shadow,
+    paddingBlock: groupStyle?.padding?.horizontal + "px",
+    paddingInline: groupStyle?.padding?.vertical + "px",
+  };
+};
+
+export default groupStyle;

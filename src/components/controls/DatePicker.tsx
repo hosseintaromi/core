2fc236353400294130ccfdefbcd58383db2dd3d9
sr-ssh <r.sadatshokouhi@gmail.com>
2023-11-15 11:23:09 +0330

    feat: :sparkles: date picker completed with validation

--- src/components/controls/DatePicker.tsx
+++ src/components/controls/DatePicker.tsx
@@ -14,26 +14,21 @@ const DatePicker: FC<DatePickerPropsType> = ({ control }) => {
   const { onChange, onBlur, name, ref } = useFBRegisterControl(control);
   const { getControlErrors } = useFBControl(control);
 
-  // if (control?.control_id) {
-  //   console.log(getValidationObject(control), getValues(control.control_id));
-  //   return (
-  //     <ControlWrapper
-  //       form={form}
-  //       formState={formState}
-  //       index={index}
-  //       controlFormState={controlFormState}
-  //     >
-  //       <input
-  //         type="date"
-  //         {...register(control.control_id, getValidationObject(control))}
-  //         {...controlRegister(control.control_id, getValidationObject(control))}
-  //       />
-  //     </ControlWrapper>
-  //   );
-  // }
   return (
     <LocalizationProvider dateAdapter={AdapterDateFnsJalali}>
-      <DateTimePicker label="Date Picker" defaultValue={new Date(2022, 1, 1)} />
+      <DateTimePicker
+        onChange={(value) =>
+          onChange({ target: { value, name: control.control_id } })
+        }
+        ref={ref}
+        slotProps={{
+          textField: {
+            onBlur,
+            name,
+            error: !!getControlErrors()?.type,
+          },
+        }}
+      />
     </LocalizationProvider>
   );
 };
--- src/components/controls/DropDown.tsx
+++ src/components/controls/DropDown.tsx
@@ -1,14 +1,12 @@
 import { FC } from "react";
 import { ControlType } from "../../@types/ControlTypes";
-import { FormControl, MenuItem, Select } from "@mui/material";
+import { MenuItem, Select } from "@mui/material";
 import { useFBRegisterControl } from "../../hooks/useFBRegisterControl";
-import { useFBControl } from "../../hooks/useFBControl";
 
 type DropDownPropsType = { control: ControlType };
 
 const DropDown: FC<DropDownPropsType> = ({ control }) => {
   const { onChange, onBlur, name, ref } = useFBRegisterControl(control);
-  const { getControlErrors } = useFBControl(control);
 
   return (
     <Select

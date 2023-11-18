8385962f5ba8fc4b7455349d1bd2186c69559810
sr-ssh <r.sadatshokouhi@gmail.com>
2023-11-18 15:49:13 +0330

    refactor: :recycle: remove border for form groups

--- src/components/styles/BackgroundStyle.tsx
+++ src/components/styles/BackgroundStyle.tsx
@@ -6,24 +6,26 @@ interface BackgroundStyleProps extends BoxProps {
 
 const BackgroundStyle = styled(Box, {
   shouldForwardProp: (prop) => prop !== "backgroundStyles",
-})<BackgroundStyleProps>(({ backgroundStyles }) => ({
-  position: "absolute",
-  inset: 0,
-  backgroundColor: backgroundStyles?.color,
-  backgroundImage: `url(${backgroundStyles?.image_url})`,
-  opacity: backgroundStyles?.image_opacity,
-  ...(backgroundStyles?.image_align &&
-  backgroundStyles?.image_align === ImageAlignEnum.Left
-    ? { backgroundPosition: "left" }
-    : backgroundStyles?.image_align === ImageAlignEnum.Center
-    ? { backgroundPosition: "center" }
-    : backgroundStyles?.image_align === ImageAlignEnum.Right
-    ? { backgroundPosition: "right" }
-    : backgroundStyles?.image_align === ImageAlignEnum.Repeat
-    ? { backgroundRepeat: "repeat" }
-    : backgroundStyles?.image_align === ImageAlignEnum.Fit
-    ? { backgroundSize: "cover" }
-    : {}),
-}));
+})<BackgroundStyleProps>(({ backgroundStyles }) => {
+  const imageAlign = backgroundStyles?.image_align;
+  return {
+    position: "absolute",
+    inset: 0,
+    backgroundColor: backgroundStyles?.color,
+    backgroundImage: `url(${backgroundStyles?.image_url})`,
+    opacity: backgroundStyles?.image_opacity,
+    ...(imageAlign && imageAlign === ImageAlignEnum.Left
+      ? { backgroundPosition: "left" }
+      : imageAlign === ImageAlignEnum.Center
+      ? { backgroundPosition: "center" }
+      : imageAlign === ImageAlignEnum.Right
+      ? { backgroundPosition: "right" }
+      : imageAlign === ImageAlignEnum.Repeat
+      ? { backgroundRepeat: "repeat" }
+      : imageAlign === ImageAlignEnum.Fit
+      ? { backgroundSize: "cover" }
+      : {}),
+  };
+});
 
 export default BackgroundStyle;

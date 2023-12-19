5c252df9ae77195dcbece20efe79d824db21a47c
sr-ssh <r.sadatshokouhi@gmail.com>
2023-12-19 18:12:11 +0330

    feat: :sparkles: add observer and context event hooks

--- src/components/samples/ChatItem.tsx
+++ src/components/samples/ChatItem.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { useObservable } from "../../hooks/useObservable";
-import { Chat, observables } from "../../stores/observable-objects";
+import { useObservable } from "../../hooks";
+import useContextEvents from "../../hooks/useContextEvents";
+import { Chat, Message, chatObservable } from "../../stores";
+import { useEffect } from "react";
 
-export function ChatItem({ chat }: { chat: Chat }) {
-	const _chat = useObservable(observables.chat, chat);
+function ChatItem({ chat, message }: { chat: Chat; message: Message }) {
+	const _chat = useObservable(chatObservable, chat);
 
-	return <h1>{_chat.name}</h1>;
+	const { listen, call } = useContextEvents<{ onReady: "onReady" }, any>(
+		{} as any
+	);
+	useEffect(() => {
+		listen({ onReady: (data: any) => {} });
+		call.onReady?.({ name: "ali" });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	return (
+		<>
+			<h1>{_chat.name}</h1>
+		</>
+	);
 }
+
+export default ChatItem;
--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -1,16 +1,44 @@
-import { Chat, observables } from "./observable-objects";
+import { Observable } from "./observable";
+
+class ChatObservable extends Observable<Chat> {
+  getId(chat: Chat) {
+    return chat.id;
+  }
+}
+export const chatObservable = new ChatObservable();
 
 const chats: Chat[] = [
   { id: "1", name: "ali" },
   { id: "2", name: "reza" },
 ];
 
+export interface Chat {
+  id: string;
+  name: string;
+}
+
 export function getChats() {
   return chats;
 }
 
-export function updateTest() {
+export function chatUpdate() {
   const chat = chats[1];
   chat.name = "Hasan";
-  observables.chat.emit("Update", chat);
+  chatObservable.update(chat);
 }
+
+export interface ChatInput {
+  id: string;
+}
+
+export interface ChatOutput {
+  id: string;
+  name: string;
+}
+
+export const apiPost =
+  <T, U>(method: string) =>
+    (input: T) =>
+      Promise.resolve<U>({} as U);
+
+export const getChatsApi = apiPost<ChatInput, ChatOutput>("getChatsApi");

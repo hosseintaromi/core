b70e0758dbfac9b985764804a509219b1648543e
sr-ssh <r.sadatshokouhi@gmail.com>
2023-12-19 14:33:53 +0330

    fix: :bug: fix menu import

--- src/hooks/useOverlayMenu.ts
+++ src/hooks/useOverlayMenu.ts
@@ -1,8 +1,8 @@
-import Menu, { MenuOptions, Option } from "../components/common-views/Menu";
+import { MenuOptions, Option, Menu } from "../components/common-views/Menu";
 import { OverlayData, useOverlay } from "./useOverlay";
 
 export interface OverlayMenuConfig
-  extends Omit<OverlayData<MenuOptions, Option>, "component"> {}
+  extends Omit<OverlayData<MenuOptions, Option>, "component"> { }
 
 export const useOverlayMenu = (menuData: OverlayMenuConfig) => {
   const elRefMenu = useOverlay<MenuOptions, Option>({
